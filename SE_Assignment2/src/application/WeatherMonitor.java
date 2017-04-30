31921e2e90570565fab59f7a33504d909c61e39c
Gwom HP <Gwom HP@GwomHP-HP>
2017-04-30 20:10:38 +0200

    Added WeatherMonitor Class

---
+++ SE_Assignment2/src/application/WeatherMonitor.java
@@ -0,0 +1,14 @@
+package application;
+
+
+public abstract class WeatherMonitor extends Monitor{
+
+	protected final int TIMESTAMPINDEX = 0;
+	protected final int TEMPERATUREINDEX = 1;
+	protected final int RAINFALLINDEX =1;
+	
+	public abstract String getLocation();
+
+	
+	
+}
