b46ec7598ab695a1a0a4c5ebb0cd52f88bcd5d96
Gwom HP <Gwom HP@GwomHP-HP>
2017-04-30 18:41:14 +0200

    Added the Main, Monitor, RainfallMonitor, TemperatureMonitor, WeatherMonitor classes and the style file (css)

---
+++ src/application/Monitor.java
@@ -0,0 +1,10 @@
+package application;
+
+
+public abstract class Monitor {
+	
+	
+	public abstract String getTime();
+	
+
+}
---
+++ src/application/RainfallMonitor.java
@@ -0,0 +1,47 @@
+package application;
+
+public class RainfallMonitor extends WeatherMonitor {
+	
+	String location, rainfall, time, placeholder;
+	
+	
+	public RainfallMonitor(String location, String [] rainfall){
+		this.location = location;
+		this.rainfall = rainfall [RAINFALLINDEX];
+		this.time = rainfall[TIMESTAMPINDEX];
+		placeholder = "-";
+	}
+	
+	
+	public void setRainfall(String [] rain){
+		rainfall = rain[ RAINFALLINDEX ];
+	}
+	
+	public void setTime(String [] temp){
+		time = temp[TIMESTAMPINDEX];
+	}
+	
+	
+	public String getLocation(){
+		return location;
+	}
+	
+	public String getRainfall(){
+		return rainfall;
+	}
+
+	@Override
+	public String getTime() {
+		return time;
+	}
+	
+	public String toString(){
+		return location + " " +  rainfall + " " + placeholder + " " + time;
+	}
+	
+	
+	
+	
+	
+
+}
