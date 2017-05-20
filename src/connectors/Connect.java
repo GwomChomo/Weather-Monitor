9b4762a893bfb1561ed3859574d3ccd3b2cfb393
Gwom HP <cmgwo1@student.monash.edu>
2017-05-20 19:40:15 +0200

    Remove all

--- src/connectors/Connect.java
+++
@@ -1,9 +0,0 @@
-package connectors;
-
-import java.rmi.RemoteException;
-
-import melbourneweather2.ExceptionException;
-
-public abstract class Connect {
-		public abstract void getData(String location) throws RemoteException, ExceptionException;
-}
